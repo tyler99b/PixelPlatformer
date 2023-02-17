8a50831631c9e0025407901fc1cd4d3ae3501dde
Tyler <bejel99t@gmail.com>
2023-02-17 02:43:41 -0500

    Added animations and allowed for swapping character skins

--- Player.gd
+++ Player.gd
@@ -8,50 +8,55 @@ extends CharacterBody2D
 @export var FRICTION = 15
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animatedSprite = $AnimatedSprite2D
+
+func _ready():
+	animatedSprite.frames = load("res://PlayerBlueSkin.tres")
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	apply_gravity()
 	
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
+	# Handle walking and friction
 	if input.x == 0:
+		animatedSprite.animation = "idle"
 		apply_friction()
 	else:
+		animatedSprite.animation = "walk"
 		apply_acceleration(input.x)
-	# Handle Jump.
+		
+		# Flip sprite based on direction of movement
+		if input.x > 0:
+			animatedSprite.flip_h = true
+		elif input.x < 0:
+			animatedSprite.flip_h = false
+	
+	# Handle jump
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
 	else:
+		animatedSprite.animation = "jump"
 		if Input.is_action_just_released("ui_up") and velocity.y < MIN_JUMP_VELOCITY:
 			velocity.y = MIN_JUMP_VELOCITY
 		
+		# Apply fast fall gravity
 		if velocity.y > 0:
 			velocity.y += 4
-			
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-	#	velocity.x = direction * SPEED
-		$AnimatedSprite2D.flip_h = velocity.x > 0
-	#else:	
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	if velocity.length() > 0:
-		$AnimatedSprite2D.animation = "walk"
-		
-	else:
-		$AnimatedSprite2D.animation = "idle"
-		
-	
-	$AnimatedSprite2D.play()
-
+	var was_in_air = not is_on_floor()
 	move_and_slide()
-
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		animatedSprite.animation = "walk"
+		animatedSprite.frame = 1
+		
+	animatedSprite.play()
+	
 func apply_gravity():
 	if not is_on_floor():
 		velocity.y += 4
